3d8fac1f3d828ebb0edd4cdf766e4f3f33818bbe
xav-ie <xruizify@gmail.com>
2023-11-19 22:58:56 -0500

    feat(auth):

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(() => {
   return {
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    optimizeDeps: { include: ['@auth/core'] },
     dev: {
       headers: {
         "Cache-Control": "public, max-age=0",
